e32b341242036f3f4ecf0489069bc61162440040
Jenna <xujenna@gmail.com>
2018-08-10 00:06:31 -0500

    added CBT app, speak memory, new nav

--- new/ITP/PupilTracker/index.php
+++ new/ITP/PupilTracker/index.php
@@ -6,13 +6,13 @@
 <div id="description_text"><img class="img-responsive" src="Screen-Shot-2018-02-07-at-12.26.51-PM.png"><br>
 <p>Pupil dilation is known to be correlated with mental exertion; for this two-part mind-wandering study,
 	I used <a href="https://pupil-labs.com/" target="new">Pupil Labs</a>, an open-source eye tracking platform, to track my pupil diameter and
-	gaze positions in two vastly different environments: 1) <a href="https://bl.ocks.org/xujenna/raw/9ef769c72cd03807c2857f49fdef1cfb/" target="new">on the floor of my department on PB&J day</a>, and 2) alone at home. </p><p>
+	gaze positions in two vastly different environments: 1) <a href="https://bl.ocks.org/xujenna/raw/9ef769c72cd03807c2857f49fdef1cfb/" target="new">on the floor of my department on PB&J day</a>, and 2) <a href="https://bl.ocks.org/xujenna/raw/6a0c20affcda7335bc0ef84d8d5b70c0/" target="new">alone at home</a>. </p><p>
 I was particularly interested to see my eye movements, as my recently developed deficit in attention requires me to read sentences,
 and sometimes even entire paragraphs, multiple times after realizing that I’ve looked at the words without actually processing them in the slightest.</p>
 <p>The extremely granular data that Pupil Labs spits out is not very informative out of context,
 	so I decided to throw together p5 sketches (with d3 support) to animate the movements over time, and add in the corresponding diameter data.</p>
 	<p><a href="https://bl.ocks.org/xujenna/raw/9ef769c72cd03807c2857f49fdef1cfb/" target="new">See the final(ish) visualization for pb&j day.</a></p>
-
+	<p><a href="https://bl.ocks.org/xujenna/raw/6a0c20affcda7335bc0ef84d8d5b70c0/" target="new">See the final(ish) at-home visualization.</a></p>
 <p>Screenshot of initial sketch result for reading on pb&j day:</p>
 	<img class="img-responsive" src="Screen-Shot-2018-02-08-at-12.58.59-PM.png">
 	<p>Screenshot of initial sketch result for reading at home:</p>
